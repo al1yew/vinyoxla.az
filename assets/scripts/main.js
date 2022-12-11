f9b41ab45a61998421ceb846c564d6e1d211d7a8
al1yew <vasifja@code.edu.az>
2022-12-11 22:53:01 +0400

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,21 @@
+toastr.options = {
+    hideDuration: 300,
+    timeOut: 2500,
+    positionClass: "toast-bottom-right",
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": false,
+    "preventDuplicates": false,
+    "onclick": null,
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+
+$(document).ready(function () {
+
+    
+
+});
