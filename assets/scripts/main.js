fb6b982d07ee4e96c150dadcebddc4b4487c5d62
al1yew <vasifja@code.edu.az>
2022-12-13 12:35:48 +0400

    commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -18,11 +18,34 @@ $(document).ready(function () {
 
     //input toUpperCase
 
-    // $(document).on('keyup', '#vincodeinput', function () {
-    //     let value = $(this).val().toUpperCase();
+    $(document).on('keyup', '#vincodeinput', function () {
+        let value = $(this).val().toUpperCase();
 
-    //     $(this).val(value);
-    // })
+        $(this).val(value);
+    });
+
+    //sample fetch
+
+    $(document).on('submit', '#vincodeform', function (e) {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+
+        let vinCode = formData.get('vincodeinput');
+
+        //WAUDG74F25N111998
+        //4T1BG22K9YU930834
+
+        console.log(vinCode);
+
+        fetchApi(vinCode)
+    })
 
 
 });
+
+async function fetchApi(vinCode) {
+    const response = await fetch(`https://api.allreports.tools/wp-json/v1/get_report_check/${vinCode}`);
+    const obj = await response.json();
+    console.log(obj);
+    return obj;
+}
